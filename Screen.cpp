4a1cf4429bb18cc0d8a9d858d9bfd5e5a6a6a8e7
Takesomelight <riveredrain@gmail.com>
2016-08-13 13:50:40 +0300

    Add files via upload

--- Screen.cpp
+++ Screen.cpp
@@ -1,33 +1,64 @@
+#include <GL\freeglut.h>
 #include <windows.h>
 #include <stdio.h>
+
+
+HDC hScreenDC, hMemoryDC;
+HBITMAP hBitmap;
+BITMAPINFOHEADER bmi;
+HGDIOBJ old;
+
+char* ScreenData;
+int w, h;
+
+void display()
+{
+
+}
+
+void timer(int value)
+{
+	BitBlt(hMemoryDC, 0, 0, w, h, hScreenDC, 0, 0, SRCCOPY);
+	GetDIBits(hMemoryDC, hBitmap, 0, h, ScreenData, (BITMAPINFO*)&bmi, DIB_RGB_COLORS);
+	char buff;
+	for (int i = 0; i < 3 * w*h; i += 3)
+	{
+		buff = ScreenData[i];
+		ScreenData[i] = ScreenData[i + 2];
+		ScreenData[i + 2] = buff;
+	}
+
+	glDrawPixels(w, h, GL_RGB, GL_UNSIGNED_BYTE, ScreenData);
+	glFlush();
+	glutTimerFunc(1, timer, 1);
+}
 
 int main(int argc, char** argv)
 {
 
-	HDC hScreenDC = GetDC(NULL);
-	int w = GetDeviceCaps(hScreenDC, HORZRES);
-	int h = GetDeviceCaps(hScreenDC, VERTRES);
-	HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
+	hScreenDC = GetDC(NULL);
+	w = GetDeviceCaps(hScreenDC, HORZRES);
+	h = GetDeviceCaps(hScreenDC, VERTRES);
+	hMemoryDC = CreateCompatibleDC(hScreenDC);
+	hBitmap = CreateCompatibleBitmap(hScreenDC, w, h);
+	SelectObject(hMemoryDC, hBitmap);
 
-	
+	bmi = { 0 };
+	bmi.biSize = sizeof(BITMAPINFOHEADER);
+	bmi.biPlanes = 1;
+	bmi.biBitCount = 24;
+	bmi.biWidth = w;
+	bmi.biHeight = h;
+	bmi.biSizeImage = 3 * w * h;
+	bmi.biCompression = BI_RGB;
 
-	HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, w, h);
-	HGDIOBJ old_obj = SelectObject(hMemoryDC, hBitmap);
-	BitBlt(hMemoryDC, 0, 0, w, h, hScreenDC, 0, 0, SRCCOPY);
+	ScreenData = (char*)malloc(3 * w * h);
 
-//	BITMAP bitmap;
-//	GetObject(hBitmap, sizeof(bitmap), &bitmap);
-
-	for (int y = 0; y < h; y++)
-	{
-		for (int x = 0; x < w; x++)
-			printf("%d ", GetPixel(hMemoryDC, x, y));
-		putchar('\n');
-	}
-
-	SelectObject(hMemoryDC, old_obj);
-	DeleteDC(hMemoryDC);
-	DeleteDC(hScreenDC);
-
+	glutInit(&argc, argv);
+	glutInitWindowSize(w, h);
+	glutCreateWindow("Screen");
+	glutDisplayFunc(display);
+	glutTimerFunc(10, timer, 0);
+	glutMainLoop();
 	return 0;
 }
